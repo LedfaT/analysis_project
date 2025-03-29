f85b7b69da7607357777f962c05da6356631d0b7
David Zaitsev <zaitsevdavid4@gmail.com>
2025-03-29 15:36:05 +0200

    fitch: connect docker to project

--- API/index.js
+++ API/index.js
@@ -4,10 +4,18 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+ы;
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
-  app.listen(process.env.PORT, () => {
-    console.log(`server listens on port ${process.env.PORT}`);
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`Server listens on port ${port}`);
   });
 };
 
