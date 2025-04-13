f604bfb470e9bcb5348a458b6ded86635ec2820b
David Zaitsev <zaitsevdavid4@gmail.com>
2025-04-13 12:04:46 +0300

    add bluetooth service

---
+++ API/services/bluetoothModuleService.js
@@ -0,0 +1,24 @@
+const { BluetoothModule } = require("../Entity");
+const ApiError = require("../exeptions/api-error");
+
+class bluetoothModuleService {
+  async create(title, generation, cost) {
+    const bluetoothModule = await BluetoothModule.findOne({
+      where: { title },
+    });
+
+    if (bluetoothModule) {
+      throw ApiError.BadRequest(
+        `Bluetooth module with ${title} title already exists`
+      );
+    }
+
+    await BluetoothModule.create({
+      title,
+      generation,
+      cost,
+    });
+  }
+}
+
+module.exports = new bluetoothModuleService();
