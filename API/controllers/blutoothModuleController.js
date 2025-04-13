e32a9b9ea5c1d8a543c806a316f2fa367e7b1e1d
David Zaitsev <zaitsevdavid4@gmail.com>
2025-04-13 12:05:36 +0300

    add creation function to bluetooth controler

---
+++ API/controllers/blutoothModuleController.js
@@ -0,0 +1,40 @@
+const ApiError = require("../exeptions/api-error");
+const bluetoothModuleService = require("../services/bluetoothModuleService");
+
+class BlutoothModuleController {
+  async createBlutoothModule(req, res, next) {
+    try {
+      const { title, generation, cost } = req.body;
+      await bluetoothModuleService.create(title, generation, cost);
+      return res.send("bluetooth module created");
+    } catch (e) {
+      next(e);
+    }
+  }
+  getBlutoothModule(req, res, next) {
+    try {
+    } catch (e) {
+      next(e);
+    }
+  }
+  updateBlutoothModule(req, res, next) {
+    try {
+    } catch (e) {
+      next(e);
+    }
+  }
+  deleteBlutoothModule(req, res, next) {
+    try {
+    } catch (e) {
+      next(e);
+    }
+  }
+  getAllBlutoothModules(req, res, next) {
+    try {
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+module.exports = new BlutoothModuleController();
