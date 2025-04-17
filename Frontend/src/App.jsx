ac99ee80f98606de87b488837ebef77b8139ec84
David Zaitsev <zaitsevdavid4@gmail.com>
2025-04-17 12:44:46 +0300

    fitch: add scss to project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import "./test.scss";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="bg-black">123adsas</div>;
+  return (
+    <main class="container">
+      <h1>Hello, Sass!</h1>
+      <p>This is a simple example of using Sass with HTML.</p>
+    </main>
+  );
 }
 
 export default App;
