8691d5b181ddaea0c2d4fa307c8a736cbc866aac
0Vextrek0 <yevhen1244@gmail.com>
2025-04-18 23:59:26 +0300

    add header design

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { BrowserRouter } from 'react-router-dom';
 import "./test.scss";
-
+import Header from "./components/Header/Header.jsx";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main class="container">
-      <h1>Hello, Sass!</h1>
-      <p>This is a simple example of using Sass with HTML.</p>
-    </main>
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
   );
 }
 
