0af70650418a10b87631158e14a5ad29d07d2ad3
David Zaitsev <davidgalanix@gmail.com>
2025-05-06 17:02:27 +0000

    add selection logic

--- Frontend/src/components/ui/ComponentsElements/ComponentsThirdSection/ComponentCard.jsx
+++ Frontend/src/components/ui/ComponentsElements/ComponentsThirdSection/ComponentCard.jsx
@@ -1,22 +1,16 @@
-import {
-  Card,
-  CardContent,
-  Button,
-  Typography,
-  TextField,
-} from "@mui/material";
+import { Card, CardContent, Button, Typography } from "@mui/material";
 import { thirdSectionStyles } from "./ThirdSection.styles";
 import StarIcon from "@mui/icons-material/Star";
 
-const ComponentCard = (component) => {
+const ComponentCard = ({ component }) => {
   return (
     <Card sx={thirdSectionStyles.card}>
       <CardContent className={thirdSectionStyles.cardContent}>
         <Typography variant="h6" className={thirdSectionStyles.name}>
-          {component?.title}
+          {component.title}
         </Typography>
         <Typography className={thirdSectionStyles.title}>
-          {component?.title}
+          {component.title}
         </Typography>
         <Typography variant="body2" className={thirdSectionStyles?.price}>
           ${component.cost.toFixed(2)}
